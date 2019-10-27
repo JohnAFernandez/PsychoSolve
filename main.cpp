fd479af5f4926f83a2963c87a9774bb60cf04b17
FSCyborg <jafernandez.tampabay@gmail.com>
2019-10-26 23:46:12 -0400

    Let's see where this goes

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+const integer MAX_ROWS = 100;
+const integer MAX_COLUMNS = 100;
+const integer MAX_GRID_SIZE = 10000;
+
+void process_incoming();
+
+int main() {
+  process_incoming();
+  
+  find_start();
+  
+  return 0
+}
+
+void process_incoming(){
+  ushort[MAX_GRID_SIZE]
+
+}
