e822508e248388a4530c479b313726b629617d2f
FSCyborg <jafernandez.tampabay@gmail.com>
2019-11-04 11:04:06 -0500

    Add case to fill up secondary data struct
    Much more efficient than telling the computer to search over and over again for block types.

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ const char STUCK = 5;
 const int MAX_CHOICES = MAX_GRID_SIZE * 100
 
 // block types, psychopath 1
+const char NOT_A_BLOCK = 0;
 const char START = 1;
 const char GOAL = 2;
 const char EDGE = 3;
@@ -31,14 +32,26 @@ const char DNLEFT = 10;
 // main struct to keep track of all the level info
 struct level_info {
   
-  //rows and columns
+  // rows and columns
   char row_count;
   char col_count;
-  location_info grid[MAX_GRID_SIZE];
-   
+  int grid_size;
+  char type[MAX_GRID_SIZE]; // stores info with do rows and then columns
+  
+  // secondary level info structure
+  grid_group not_block_list;
+  grid_group goals;
+  grid_group edges;
+  grid_group movas;
+  grid_group simedges;
+  grid_group holes;
+  grid_group uplefts;
+  grid_group uprights;
+  grid_group dnrights;
+  grid_group dnlefts;
+  
   //start and end
-  location start;
-  location end;
+  location start;  
     
 } level_info;
 
@@ -53,15 +66,15 @@ struct decision_point {
   char movement[MAX_CHOICES];
 } decision_point;
 
-struct location_info {
-  location coord;
-  char type;
-} location_info
-
 struct location {
   char x;
   char y;
 } location;
+
+struct grid_group {
+ int count;
+ location xy[MAX_GRID_SIZE];
+}
 
 // Header Functions
 
@@ -82,28 +95,91 @@ int main() {
 
 void process_incoming(level_info* level){
 
-  char whole_level[MAX_GRID_SIZE];
-  memset(whole_level, 0, MAX_GRID_SIZE);
-  memset(level->rows.block_type, 0, MAX_COLS); //not sure if I initialized the multidimensional arrays correctly
-  memset(level->cols.block_type, 0, MAX_ROWS);
+  memset(level.row_count, 0, MAX_GRID_SIZE);
   
   // file reading and stuffing loop should go here.....
-  //blarg, making sure that
+  //blarg, making sure that col_count and row_count are set
+  // has to be passed to the program because it can be rectangular
   
-  // now just placing it in our working structs
+  level.grid_size = level.col_count * level.row_count;
+  level.not_block_list.count = 0
+  level.goals.count = 0;
+  level.edges.count = 0;
+  level.movas.count = 0;
+  level.simedges.count = 0;
+  level.holes.count = 0;
+  level.uplefts.count = 0;
+  level.uprights.count = 0;
+  level.dnrights.count = 0;
+  level.dnlefts.count = 0;  
+    
+  // now just placing it in our level info struct
   int k = 0;
-  for(int i = 0; i<level->col_count; i++) {
-    for(int j = 0; j<row_count; j++) {
-      level_info->rows[j].block_type[i] = whole_level[k];
-      level_info->cols[i].block_type[j] = whole_level[k];
+  for(int i = 0; i < level.col_count; i++) {
+    for(int j = 0; j < level.row_count; j++) {
+      level.type[k] = //pull type from       
       k++;
+      switch(level.type[k]){
+        case 1:
+          level.start = 
+        case 2:
+          level.goals.xy[level.goals.count].x = i;
+          level.goals.xy[level.goals.count].y = j;
+          level.goals.count++;
+        case 3:  
+          level.edges.xy[levels.edges.count].x = i;
+          level.edges.xy[levels.edges.count].y = j;
+          level.edges.count++;
+        case 4: 
+          level.movas.xy[levels.movas.count].x = i;
+          level.movas.xy[levels.movas.count].y = j;
+          level.movas.count++;
+        case 5:
+          level.simedges.xy[levels.simedges.count].x = i;
+          level.simedges.xy[levels.simedges.count].y = j;
+          level.simedges.count++;
+        case 6:
+          level.holes.xy[levels.holes.count].x = i;
+          level.holes.xy[levels.holes.count].y = j;
+          level.holes.count++;
+        case 7:
+          level.uplefts.xy[levels.uplefts.count].x = i;
+          level.uplefts.xy[levels.uplefts.count].y = j;
+          level.uplefts.count++;
+        case 8:
+          level.uprights.xy[levels.uprights.count].x = i;
+          level.uprights.xy[levels.uprights.count].y = j;
+          level.uprights.count++;
+        case 9:
+          level.dnrights.xy[levels.dnrights.count].x = i;
+          level.dnrights.xy[levels.dnrights.count].y = j;
+          level.dnrights.count++;
+        case 10:
+          level.dnlefts.xy[levels.dnlefts.count].x = i;
+          level.dnlefts.xy[levels.dnlefts.count].y = k;
+          level.dnlefts.count++;
+          
+        case 0:
+        
+        default:
+          break;
+      }        
     }
   }
+  
+  for (; k < MAX_GRID_SIZE; k++) {
+     level.type[k] = 0;
+  }
+  
 }
 
-void search_level(){
- for(int i = 0);
- int j = 0;
+
+
+void search_level(bool find_all, char type){
+  for(int i = 0; i < level.col_count; i++) {
+    for(int j = 0; j < level.row_count; j++) {
+      level.type[k] = //pull type from       
+      k++;
  
  
   
