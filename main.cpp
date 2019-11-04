b4186d43eae624104e39bce7ab0a110e87bf79fb
FSCyborg <jafernandez.tampabay@gmail.com>
2019-11-04 16:01:07 -0500

    And the code is exploding....
    Adding functions to do very basic things, like tell if this is just an island of blocks.

--- main.cpp
+++ main.cpp
@@ -11,8 +11,12 @@ const char DOWN = 2;
 const char LEFT = 3;
 const char RIGHT = 4;
 const char STUCK = 5;
+const char NODE_UPRIGHT = 6;
+const char NODE_UPLEFT = 7;
+const char NODE_DNLEFT = 8;
+const char NODE_DNRIGHT = 9;
 const int MAX_CHOICES = MAX_GRID_SIZE * 100
-
+  
 // block types, psychopath 1
 const char NOT_A_BLOCK = 0;
 const char REGU = 1
@@ -54,6 +58,8 @@ struct level_info {
   grid_group dnrights;
   grid_group dnlefts;
   
+  grid_group node_incomplete;
+  
   //start and end
   location start;  
     
@@ -78,14 +84,19 @@ struct grid_group {
 } grid_group;
 
 struct decision_matrix {
-  char block[9];
+  int block[9];
 } decision_block;
 
 // Header Functions
 
 void process_incoming(level_info* level);
 
+//fills up the level with simulated edges that help the computer reduce the complexity of the level.
 void create_simedges(level_info* level);
+
+location find_areas(); // not yet written, 
+
+location move(location source, char direction); 
 
 // PROGRAM START
 
@@ -111,7 +122,7 @@ void process_incoming(level_info* level){
   // has to be passed to the program because it can be rectangular
   
   if (level->row_count < 3 || level->col_count < 3) {
-    int3();
+    int3(); // let's just start with this while I'm still new at this
   }
   
   level->grid_size = level->col_count * level->row_count;
@@ -194,24 +205,72 @@ void process_incoming(level_info* level){
   }
 }
 
-void create_simedges_whole(level){
+void create_simedges_whole(level_info* level){
   decision_matrix chunk;
-  int a_start = 0;
-  int b_start = MAX_ROWS;
-  int c_start = 2*MAX_ROWS;
+  int a_start = 0, int b_start = MAX_ROWS, c_start = 2*MAX_ROWS;
+  int i;
   
-  chunk.block[0] = level->type[a_start];
-  chunk.block[1] = level->type[a_start+1];
-  chunk.block[2] = level->type[a_start+2];
-  chunk.block[3] = level->type[b_start];  
-  chunk.block[4] = level->type[b_start+1];
-  chunk.block[5] = level->type[b_start+2];
-  chunk.block[6] = level->type[c_start];  
-  chunk.block[7] = level->type[c_start+1];
-  chunk.block[8] = level->type[c_start+2];
+  chunk.block[0] = EDGE;
+  chunk.block[1] = EDGE;
+  chunk.block[2] = EDGE;
+  chunk.block[3] = EDGE;
+  chunk.block[4] = a_start;
+  chunk.block[5] = a_start + 1;
+  chunk.block[6] = EDGE;  
+  chunk.block[7] = b_start;  
+  chunk.block[8] = b_start + 1;
   
-  
+  for (i = 0; i < 10; i++) {
+    switch (level->.type[chunk.block[i]]) {
+      case EDGE:
+      case SIMEDGE:
+        node_search()
+        break;          
+    }
+    
+  }
   
   
 }
 
+// helper function that just tells us the next location.
+location move(location original_source, char direction, level_info* level){ 
+  
+  int source = (original_source.xy.x * col_count) + original_source.xy.y;
+  
+  switch (direction)
+    case UP:
+      grid_group = node_search(original_source, level);
+        if (grid_group.count == 0){
+        }
+        }
+      }
+  
+      break;
+    case DOWN:
+        
+      break;
+    case LEFT:
+    
+      break;
+    case RIGHT:
+      break;
+}
+
+grid_group find_areas() {  
+    
+}
+        
+grid_group node_search(location initial, level_info* level) {
+  grid_group result = {};
+  for (int i = 0; i < [MAX_GRID_SIZE])
+    if (initial.x == level->node_incomplete.xy.x && initial.y == level->node_incomplete.xy.y) {
+      result.count = 0
+      return grid_group;
+    }
+  }
+}
+
+void append_grid_group {
+
+}
